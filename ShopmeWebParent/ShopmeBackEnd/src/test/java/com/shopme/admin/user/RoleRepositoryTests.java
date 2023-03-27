8839339d808711b514112e68d9d6d2dcb1ca7064
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-27 13:51:27 +0200

    Add constructors and unit test for creating role

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,28 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.shopme.common.entity.Role;
+@DataJpaTest
+//so we test with the real db
+@AutoConfigureTestDatabase(replace = NONE)
+public class RoleRepositoryTests {
+	
+	@Autowired
+	private RoleRepository repo;
+	
+	@Test
+	public void testCreateFirstRole() {
+		Role roleAdmin = new Role("Admin", "Manage everything");
+		Role savedRole = repo.save(roleAdmin);
+		assertThat(savedRole.getId()).isGreaterThan(0);
+	}
+	
+
+}
