868a2898b02c0aad50fb114eddf3d7836e99cf77
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-27 20:35:30 +0200

    Add mapping of values in form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.common.entity.User;
 
@@ -25,10 +26,21 @@ public class UserController {
 	}
 	
 	//maps to users.html
+	//map value of formfield to model / form
 	@GetMapping("/users/new")
-	public String newUser() {
+	public String newUser(Model model) {
+		User user =   new User();
+		model.addAttribute("user",user);
 		return "user_form";
 		
 	}
+	
+	//when pushing save button, map values of form fields to user objects
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		System.out.println(user);  //to string in User class
+		return "redirect:/users";
+	}
+	
 
 }
