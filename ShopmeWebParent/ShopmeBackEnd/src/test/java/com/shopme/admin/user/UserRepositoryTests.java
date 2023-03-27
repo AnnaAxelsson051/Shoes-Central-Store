549b40d6e2d1ffc35bc95c63d56d297f68987b3b
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-27 17:12:44 +0200

    Add test for adding user

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -1,10 +1,16 @@
 package com.shopme.admin.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= Replace.NONE) //to run test on real db
@@ -13,9 +19,18 @@ public class UserRepositoryTests {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private TestEntityManager entityManager;
+	
 	@Test 
 	public void testCreateUser() {
 		//creates table from user class
+		Role roleAdmin = entityManager.find(Role.class, 1);  //Id 1 = admin
+		User user1 = new User("test1@test1.com", "password", "test1FirstName", "test1LastName");
+	user1.addRole(roleAdmin);
+	
+	User savedUser = repo.save(user1);
+	assertThat(savedUser.getId()).isGreaterThan(0);
 	}
 
 }
