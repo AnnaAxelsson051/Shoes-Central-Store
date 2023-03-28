41184a3a2e455bc924b8b57dbe3cb2d13ab441b1
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-28 21:19:48 +0200

    Add new security configuration

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/MvcConfig.java
@@ -0,0 +1,15 @@
+/*package com.shopme.admin.user;
+
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig extends WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		MvcConfigurer.super.addResourceHandlers(registry);
+	}
+	
+
+}*/
