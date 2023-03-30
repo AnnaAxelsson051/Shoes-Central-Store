b2b299e960a29a39bcb16ff63348f4cb7fb6395c
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-30 22:56:08 +0200

    Add update password functionality

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common.js
@@ -7,7 +7,17 @@
 				customizeDropDownMenu();
 		});
 		
+		//Function for log out menu sliding down when mouse hover over drop down link 
 	function customizeDropDownMenu(){
+		$(".navbar .dropdown").hover(
+			function(){
+				$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+			},
+			function(){
+				$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+			//when mouse move away fro droplink - slide up, closse drop down menu
+			}
+		);
 		$(".dropdown > a").click(function(){
 			location.href = this.href;
 		});
