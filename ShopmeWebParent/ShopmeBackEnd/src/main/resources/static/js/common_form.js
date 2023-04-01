4c0757b83a1608d275832b4e25a3743a356dffd8
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-01 17:40:52 +0200

    Refractor show modal dialoge code

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -27,28 +27,16 @@ $(document).ready(function(){
 	}
 		
 	
-	/*$(document).ready(function() {
-			$("#logoutLink").on("click", function(e) {
-				e.preventDefault();
-				document.logoutForm.submit();
-			});
-			
-				customizeDropDownMenu();
-		});
-		
-		//Function for log out menu sliding down when mouse hover over drop down link 
-	function customizeDropDownMenu(){
-		$(".navbar .dropdown").hover(
-			function(){
-				$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-			},
-			function(){
-				$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-			//when mouse move away fro droplink - slide up, closse drop down menu
-			}
-		);
-		$(".dropdown > a").click(function(){
-			location.href = this.href;
-		});
+		function showModalDialog(title, message){
+		$("#modalTitle").text(title);
+		$("#modalBody").text(message);
+		$("#modalDialog").modal();
 	}
-		*/
+	
+	function showErrorModal(message){
+		showModalDialog("Error",message);
+	}
+	
+	function showWarningModal(message){
+		showModalDialog("Warning",message);
+	}
