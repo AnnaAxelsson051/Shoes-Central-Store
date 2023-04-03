50425e4eda519363e9217045f735721b202e946f
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-03 18:55:38 +0200

    Add list by page method and adjust html

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -2,6 +2,10 @@ package com.shopme.admin.brand;
 
 import com.shopme.common.entity.Brand;
 
+import java.awt.print.Pageable;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -12,4 +16,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 		public Long countById(Integer id);
 		
 		Brand findByName(String name);
+		
+		//For search for brands for name column
+		@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+		public Page<Brand> findAll(String keyword, Pageable pageable);
 }
