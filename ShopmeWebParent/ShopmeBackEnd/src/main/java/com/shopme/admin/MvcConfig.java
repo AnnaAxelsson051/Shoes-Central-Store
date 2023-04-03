067aeca344b45ea5f337618f2823e9b1d0b04a4f
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-03 19:10:24 +0200

    Refractor Mvcconfig class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -12,7 +12,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+//public class MvcConfig{
+	public class MvcConfig implements WebMvcConfigurer{
 
 	
 	//Exposes absolute path to photos so they are avaliable
@@ -20,34 +21,26 @@ public class MvcConfig {
 	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
-		//user photos
-		String dirName = "user-photos";
-		Path userPhotosDir = Paths.get(dirName);
 		
-		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/" + dirName + "/**")
-		.addResourceLocations("file:/" + userPhotosPath + "/");
-	
+	//User photos
+	exposeDirectory("user-photos", registry);
 	
 	//Item images
-	String categoryImagesDirName = "../category-images";
-	Path categoryImagesDir = Paths.get(categoryImagesDirName);
-	
-	String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-	
-	registry.addResourceHandler("/category-images/**")
-	.addResourceLocations("file:/" + categoryImagesPath + "/");
-	
+	exposeDirectory("../category-images", registry);
 	
 	//Brand logos
-	String brandLogosDirName = "../brand/logos";
-	Path brandLogosDir = Paths.get(brandLogosDirName);
+	exposeDirectory("../brand-logos", registry);
+
+	}
 	
-	String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
-	
-	registry.addResourceHandler("/brand-logos/**")
-	.addResourceLocations("file:/" + brandLogosPath + "/");
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
+		
+		String logicalPath = pathPattern.replace("..","") + "/**";
+		
+		registry.addResourceHandler(logicalPath)
+		.addResourceLocations("file:/" + absolutePath + "/");
 	}
 	//DEPRECATED
 
