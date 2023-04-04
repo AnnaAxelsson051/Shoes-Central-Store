b564fe7cb3db45da0c9d3622e0eea79d45b6dbee
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-04 13:23:08 +0200

    Add functionality for sumbission fo product form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 import com.shopme.admin.brand.BrandService;
@@ -39,5 +39,15 @@ public class ProductController {
 		model.addAttribute("pageTitle", "Create New Product");
 		
 		return "products/product_form";
+	}
+	
+	//Handeler method for submission of product form
+	@PostMapping("/products/save)")
+	public String saveProduct(Product product) {
+		System.out.println("Product Name: " + product.getName());
+		System.out.println("Brand ID: " + product.getBrand().getId());
+		System.out.println("Category ID: " + product.getCategory().getId());
+	
+	return "redirect:/products";
 	}
 }
