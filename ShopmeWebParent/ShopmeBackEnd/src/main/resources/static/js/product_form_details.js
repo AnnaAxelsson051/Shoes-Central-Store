3c06294ad9d8a3098eab3c757b945de4c3470953
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-05 13:11:56 +0200

    Modify functionality for adding details, add delete detail functionality

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_details.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_details.js
@@ -1,6 +1,10 @@
 function addNextDetailSection(){
+	
+	allDivDetails = $("[id^='divDetail']");
+	divDetailsCount = allDivDetails.length;
+	
 	htmlDetailSection = `
-	<div class="form-inline">
+	<div class="form-inline" id="divDetail${nextDivDetailsCount}">
 		<label class="m-3">Name:</label>
 		<input type="text" class="form-control w-25" name="detailNames" maxLength="255">
 	    <label class="m-3">Value:</label>
@@ -9,4 +13,20 @@ function addNextDetailSection(){
 	`;
 	
 	$("#divProductDetails").append(htmlDetailSection);
+	
+	 previousDivDetailSection = allDivDetails.last();
+	 previousDivDetailID =  previousDivDetailSection.attr("id");
+	 
+    htmlLinkRemove =`
+			<a class="btn fas fa-times-circle fa-2x icon-dark" 
+			href="javascript:removeDetailSectionById('${previousDivDetailID}')"
+			title="Remove this detail"></a>;
+			`;
+			
+    previousDivDetailSection = allDivDetails.last();
+    previousDivDetailSection.append(htmlLinkRemove);
+}
+
+function removeDetailSectionById(id){
+	$("#divDetail" + id).remove();
 }
