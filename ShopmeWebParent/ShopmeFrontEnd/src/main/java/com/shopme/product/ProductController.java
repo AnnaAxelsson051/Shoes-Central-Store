7e3004abf82a41b7dfe94e2af9096ed940cc704b
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-07 15:58:55 +0200

    Modify functionality for displaying product categories by alias

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -0,0 +1,28 @@
+package com.shopme.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.shopme.common.entity.Category;
+import com.shopme.category.CategoryService;
+
+@Controller
+public class ProductController {
+
+	@Autowired private CategoryService categoryService;
+	
+	//handeling requests for viewing product in category and product details
+	//List product in category, if category not found displays customized
+	//errormessage
+	@GetMapping("/c/category_alias}")
+	public String viewCategory(@PathVariable("category_alias") String alias, Model model) {
+	Category category = categoryService.getCategory(alias);
+	if (category == null) {
+		return "error/404";
+	}
+		model.addAttribute("pageTitle", category.getName());
+		return "products_by_category";
+	}
+}
