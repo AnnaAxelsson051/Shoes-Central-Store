b0a515b94fc6a102acc47b16d3485398fdb39775
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-07 15:18:56 +0200

    Add functionality for displaying category imahes on homepage

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MainController.java
@@ -1,13 +1,27 @@
 package com.shopme;
 
+import java.util.List;
+import com.shopme.common.entity.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.category.CategoryService;
+
+import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
 	
+	@Autowired private CategoryService categoryService;
+	
+	//Adding list of enabled categories sorted in ASC onto model
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(Model model) {
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+		model.addAttribute("listCategories", listCategories);
+		
 		return "index";
 	}
 
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/MvcConfig.java
@@ -0,0 +1,61 @@
+package com.shopme;
+
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//public class MvcConfig{
+	public class MvcConfig implements WebMvcConfigurer{
+
+	
+	//Exposes absolute path to photos so they are avaliable
+	//for the webclient browser to access
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	
+	exposeDirectory("../category-images", registry);
+	
+	exposeDirectory("../brand-logos", registry);
+	
+	exposeDirectory("../product-images", registry);
+
+	}
+	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
+		
+		String logicalPath = pathPattern.replace("../","") + "/**";
+		
+		registry.addResourceHandler(logicalPath)
+		.addResourceLocations("file:/" + absolutePath + "/");
+	}
+	//DEPRECATED
+
+//@Configuration
+//public class MvcConfig extends WebMvcConfigurer {
+	
+	//Exposes absolute path to user photos so they are visible in browser
+	//@Override
+	//public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	//	String dirName = "user-photos";
+	//	Path userPhotosDir = Paths.get(dirName);
+		
+	//	String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
+		
+	//	registry.addResourceHandler("/" + dirName + "/**")
+	//	.addResourceLocations("file:/" + userPhotosPath + "/");
+//	}
+	
+	
+	
+
+}
