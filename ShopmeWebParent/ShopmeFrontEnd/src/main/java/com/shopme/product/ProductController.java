411326d3c459044b19788ad8e3a45d6a8c02fb9b
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-07 18:44:15 +0200

    Modify controller functionality for pagination

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -3,30 +3,52 @@ package com.shopme.product;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.shopme.common.entity.Category;
+import com.shopme.common.entity.Product;
 import com.shopme.category.CategoryService;
 
 @Controller
 public class ProductController {
 
+	@Autowired private ProductService productService;
 	@Autowired private CategoryService categoryService;
 	
-	//handeling requests for viewing product in category and product details
+	//handeling requests for viewing product in category by id and product details
 	//List product in category, if category not found displays customized
 	//errormessage
 	@GetMapping("/c/category_alias}")
-	public String viewCategory(@PathVariable("category_alias") String alias, Model model) {
+	public String viewCategory(
+			@PathVariable("category_alias") String alias, 
+			@PathVariable("pageNum") int pageNum,
+			Model model) {
 	Category category = categoryService.getCategory(alias);
 	if (category == null) {
 		return "error/404";
 	}
 	    List<Category> listCategoryParents = categoryService.getCategoryParents(category);
-		model.addAttribute("pageTitle", category.getName());
-		model.addAttribute("listCategoryParents", listCategoryParents);
+		
+	    Page<Product> pageProducts = productService.listByCategory(1, category.getId());
+	    List<Product> listProducts = pageProducts.getContent();
+	    
+		long startCount=(pageNum -1) * ProductService.PRODUCTS_PER_PAGE +1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE -1;
+	if(endCount > pageProducts.getTotalElements()) {
+		endCount = pageProducts.getTotalElements();
+	}
+	
+	model.addAttribute("currentPage", pageNum);
+	model.addAttribute("totalPages", pageProducts.getTotalPages());
+	model.addAttribute("startCount", startCount);
+	model.addAttribute("endCount", endCount);
+	model.addAttribute("totalItems", pageProducts.getTotalElements());
+    model.addAttribute("pageTitle", category.getName());
+	model.addAttribute("listCategoryParents", listCategoryParents);
+	model.addAttribute("listProducts", listProducts);
 		
 		return "products_by_category";
 	}
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductService.java
@@ -0,0 +1,24 @@
+package com.shopme.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
+
+import com.shopme.common.entity.Product;
+
+@Service
+public class ProductService {
+public static final int PRODUCTS_PER_PAGE = 10;
+
+@Autowired private ProductRepository repo;
+
+//Finds category by category id
+public Page<Product> listByCategory(int pageNum, Integer categoryId){
+	String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
+	Pageable pageable = PageRequest.of(pageNum, PRODUCTS_PER_PAGE);
+	
+	return repo.listByCategory(categoryId, categoryIdMatch, pageable);
+}
+}
