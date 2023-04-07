7594f530136b48783d79317d952c64a32175306b
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-07 11:48:42 +0200

    Update functionality so log in page only shows for not logged in users

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -1,7 +1,10 @@
 package com.shopme.admin;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 
 @Controller
 public class MainController {
@@ -12,9 +15,15 @@ public class MainController {
 		return "index";
 	}
 	
+	//If user is not logged in return login site otherwose 
+	//redirect to homepage
 	@GetMapping("/login")
 	public String viewLoginPage() {
-		return "login";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+			return "login";	
+		}
+		return "redirect:/";
 	}
 
 }
