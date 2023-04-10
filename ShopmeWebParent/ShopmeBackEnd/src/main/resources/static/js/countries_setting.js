a72fb229c1e930722b1eecfc3fc484a599ca5919
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-10 15:38:40 +0200

    Add functionality for updating country

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_setting.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_setting.js
@@ -25,7 +25,100 @@ $(document).ready(function(){
 	dropDownCountry.on("change",function(){
 		changeFormStateToSelectedCountry();
 	});
+	
+	//If value on the btn is add calling addCountry function
+	//Otherwise changing the form state to new
+	buttonAddCountry.click(function(){
+		if(buttonAddCountry.val() == "Add"){
+			addCountry();
+		}else{
+			changeFormStateToNew();
+		}
+	});
+	
+	buttonUpdateCountry.click(function(){
+		updateCountry();
+	})
 });
+
+function updateCountry(){
+	url = contextPath + "countries/save";
+	countryName = fieldCountryName.val();
+	countryCode = fieldCountryCode.val();
+	
+	countryID = dropDownCountry.val().spit("-")[0];
+	
+	jsonData = {id: countryId, name:countryName, code};
+	
+	$.ajax({
+		type: 'POST',
+		url: url,
+	    beforeSend:function(xhr){
+		xhr.setRequestHeader(csrfHeaderName, csrfValue);
+	},
+	data: JSON.stringify(jsonData),
+	contentType: 'application/json'
+	}).done(function(countryId){
+		 $("#dropDownCountries option:selected").text(countryId + "-" + countruCode);
+		 $("#dropDownCountries option:selected").text(countryName);
+		showToastMessage("The country has been updated");
+	
+	changeFormStateToNew();
+	
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error")
+	});
+}
+
+//Making an ajax call to server to the countries/save urö
+//Getting country name and code from the textfields
+//creating a json obj that will be sent with the request with key n val
+//Tru security check, vonverting json obj to string
+function addCountry(){
+	url = contextPath + "countries/save";
+	countryName = fieldCountryName.val();
+	countryCode = fieldCountryCode.val();
+	jsonData = {name:countryName, code};
+	
+	$.ajax({
+		type: 'POST',
+		url: url,
+	    beforeSend:function(xhr){
+		xhr.setRequestHeader(csrfHeaderName, csrfValue);
+	},
+	data: JSON.stringify(jsonData),
+	contentType: 'application/json'
+	}).done(function(countryId){
+		selectNewlyAddedCountry(countryId, countryCode, countryName);
+		showToastMessage("The new country has been added");
+	}).fail(function(){
+		showToastMessage("ERROR: Could not connect to server or server encountered an error")
+	});
+}
+
+
+function selectNewlyAddedCountry(countryId, countryCode, countryName){
+	optionValue = countryId + "-" + countryCode;
+	$("<option>").val(optionValue).text(countryName).appendTo(dropDownCountry);
+$("#dropDownCountries option[value='" + optionValue + "']").prop("selected", true);
+fieldCountryCode.val("");
+fieldCountryName.val("").focus();
+}
+//Altering text on buttons from new to add when clicking
+//a country in the list and clicking new btn
+//New is for enterig values, Add is for saving values to db
+//Toasmessage appering after adding, country appears in list
+//and fields are cleared
+function changeFormStateToNew(){
+	buttonAddCountry.val("Add");
+	labelCountryName.text("Country Name:");
+	
+    buttonEditCountry.prop("disabled", true);
+	buttonDeleteCountry.prop("disabled", true);
+
+fieldCountryCode.val("");
+fieldCountryName.val("").focus();
+}
 
 //Handeling what buttons are clickeable
 //Setting values for text field, country code etc
@@ -59,7 +152,7 @@ function loadCountries(){
 		showToastMessage("All countries have been loaded")
 	}).fail(function(){
 		showToastMessage("ERROR: Could not connect to server or server encountered an error")
-	})
+	});
 }
 
 function showToastMessage(message) {
