8a31d320bb5fefcd7dc912d6b63c7cd7a57ed037
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-10 23:00:33 +0200

    Add functionality for checking if user email is unique when a new user creates account

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
 import com.shopme.setting.CountryRepository;
 
 @Service
@@ -17,8 +18,16 @@ public class CustomerService {
 	@Autowired 
 	private CustomerRepository customerRepo;
 	
-	
+	//Listing all countries in form for customer to 
+	//select from when creating account
 	public List <Country> listAllCountries(){
 		return countryRepo.findAllByOrderByNameAsc();
 	}
+	
+	//Checking if email user filled in to form is unique by 
+	// or already in system
+	public boolean isEmailUnique(String email) {
+		Customer customer = customerRepo.findByEmail(email);
+		return customer == null;
+;	}
 }
