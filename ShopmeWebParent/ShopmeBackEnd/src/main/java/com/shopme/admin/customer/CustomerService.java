46c2c3a1289be10a6ec8a12ad32cf3901e4510da
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-13 20:53:24 +0200

    Add forgot password form

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
@@ -90,7 +90,7 @@ public class CustomerService {
 		customerInForm.setCreatedTime(customerInDB.getCreatedTime());
 		customerInForm.setVerificationCode(customerInDB.getVerificationCode());
 	    customerInForm.setAuthenticationType(customerInDB.getAuthenticationType());
-	    
+	   customerInForm.setResetPasswordToken(customerInDB.getResetPasswordToken()); 
 	customerRepo.save(customerInForm);
 }
 	public void delete (Integer id) throws CustomerNotFoundException{
