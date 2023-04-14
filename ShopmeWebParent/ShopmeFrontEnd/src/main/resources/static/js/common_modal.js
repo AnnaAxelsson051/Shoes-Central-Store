65e4eac8a6a00e92880f4780df4e457e1b8a11ec
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-14 14:40:56 +0200

    Add quantity control functionality

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_modal.js
@@ -0,0 +1,16 @@
+
+	
+	
+		function showModalDialog(title, message){
+		$("#modalTitle").text(title);
+		$("#modalBody").text(message);
+		$("#modalDialog").modal();
+	}
+	
+	function showErrorModal(message){
+		showModalDialog("Error",message);
+	}
+	
+	function showWarningModal(message){
+		showModalDialog("Warning",message);
+	}
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/quantity_control.js
@@ -0,0 +1,29 @@
+//Increasing or decreasing products when user clicks + / -
+
+$(document).ready(function(){
+	$(".linkMinus").on("click", function(evt){
+		evt.preventDefault();
+		productId = $(this).attr("pid");
+		quantityInput = $("#quantity" + productId);
+		newQuantity = parseInt(quantityInput.val()) -1;
+		
+		if (newQuantity > 0){
+			quantityInput.val(newQuantity);
+		}else{
+			showWarningModal('Minimum quantity is 1');
+		}
+	});
+	
+		$(".linkPlus").on("click", function(evt){
+	    evt.preventDefault();
+		productId = $(this).attr("pid");
+		quantityInput = $("#quantity" + productId);
+		newQuantity = parseInt(quantityInput.val()) +1;
+		
+		if (newQuantity <= 5){
+			quantityInput.val(newQuantity);
+		}else{
+			showWarningModal('Maximum quantity is 5');
+		}
+	});
+});
