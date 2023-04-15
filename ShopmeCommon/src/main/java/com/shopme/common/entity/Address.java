032aa802acd9f3bf311ab126668010edc6fe3326
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-15 18:08:34 +0200

    Add address entity class, addres repo interface and tests for address repo

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Address.java
@@ -0,0 +1,161 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+	
+	@Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+	
+	@Column(name = "phone_number", nullable = false, length = 15)
+    private String phoneNumber;
+	
+	@Column(name = "address_line_1", nullable = false, length = 64)
+    private String addressLine1;
+	
+	@Column(name = "address_line_2", nullable = false, length = 64)
+    private String addressLine2;
+	
+	@Column(nullable = false, length = 45)
+    private String city;
+	
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public boolean isDefaultForShipping() {
+		return defaultForShipping;
+	}
+
+	public void setDefaultForShipping(boolean defaultForShipping) {
+		this.defaultForShipping = defaultForShipping;
+	}
+
+	@Column(nullable = false, length = 45)
+    private String state;
+
+	@Column(name = "postal_code", nullable = false, length = 10)
+    private String postalCode;
+	
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
+	@Column(name = "default_address")
+	private boolean defaultForShipping;
+
+	
+	@Override
+	public String toString() {
+		return "Address [Id=" + Id + ", firstName=" + firstName + ", lastName=" + lastName + ", phoneNumber="
+				+ phoneNumber + ", addressLine1=" + addressLine1 + ", addressLine2=" + addressLine2 + ", city=" + city
+				+ ", state=" + state + ", postalCode=" + postalCode + ", country=" + country + ", customer=" + customer
+				+ ", defaultForShipping=" + defaultForShipping + "]";
+	}
+	
+}
