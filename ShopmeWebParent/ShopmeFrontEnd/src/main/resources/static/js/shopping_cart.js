490e7b85b18a061f933fe6f12c1befdbc63122b7
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-15 11:38:17 +0200

    Modify functionality for removing products from cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
@@ -70,13 +70,24 @@ function increaseQuantity(link){
 		function updateTotal(){
 			total = 0.0;
 			$(".subtotal").each(function(index, element){
+			productCount ++;
 				total += parseFloat(element.innerHTML.replaceAll(",", ""));
 			});
 			
+			if(productCount < 0){
+				showEmptyShoppingCart();
+			} else{
 			formattedTotal = $.number(total, 2);
 			$("#total").text(formattedTotal);
+			}
 		}
 		
+		
+			function showEmptyShoppingCart(){
+			$("#sectionTotal").hide();
+			$("#sectionEmptyCartMessage").removeClass("d-none");
+			
+		}
 		
 		
 		function removeProduct(){
@@ -92,6 +103,7 @@ function increaseQuantity(link){
             rowNumber = link.attr("rowNumber");
             removeProductHTML(rowNumber);
             updateTotal();
+            updateCountNumbers();
             showModalDialog("Shopping Cart", response);
             }).fail(function(){
 	        showErrorModal("Error while removing product.");
@@ -100,5 +112,15 @@ function increaseQuantity(link){
 		
 		    function removeProductHTML(rowNumber){
 			$("#row" + rowNumber).remove();
+			$("#blankLine" + rowNumber).remove();
 		    }
+		    
+		//Updating count of products in cart when customer add or removed    
+		//products
+		function updateCountNumbers(){
+			$(".divCount").each(function(index, element){
+				element.innerHTML = "" + (index + 1);
+			});
+		}
 		
+	
