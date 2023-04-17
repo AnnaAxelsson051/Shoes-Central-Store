b04bf38c45161189e9a1e0663014a3152669c3a1
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-17 16:36:55 +0200

    Modify functionality for calculating total sumb in cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
@@ -8,10 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.Utility;
+import com.shopme.address.AddressService;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
-import com.shopme.common.exception.CustomerNotFoundException;
+import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.Address;
 import com.shopme.customer.CustomerService;
+import com.shopme.shipping.ShippingRateService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -20,8 +23,10 @@ public class ShoppingCartController {
 	
 @Autowired private ShoppingCartService cartService;
 @Autowired private CustomerService customerService;
+@Autowired private AddressService addressService;
+@Autowired private ShippingRateService shipService;
 
-//Callculating total sumb for whats in cart
+//Callculating total sumb for whats in cart including sshipping rate
 @GetMapping("/cart")
 public String viewCart(Model model, HttpServletRequest request) {
 	Customer customer = getAuthenticatedCustomer(request);
@@ -33,6 +38,19 @@ public String viewCart(Model model, HttpServletRequest request) {
 		estimatedTotal += item.getSubTotal();
 	}
 	
+	Address defaultAddress = addressService.getDefaultAddress(customer);
+	ShippingRate shippingRate = null;
+	boolean usePrimaryAddressAsDefault = false;
+	
+	if(defaultAddress != null){
+		shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+	}else {
+		usePrimaryAddressAsDefault = true;
+		shippingRate = shipService.getShippingRateForCustomer(customer);
+	}
+	
+	model.addAttribute("usePrimaryAddressAsDefault", usePrimaryAddressAsDefault);
+	model.addAttribute("shippingSupported", shippingRate != null);
 	model.addAttribute("cartItems", cartItems);
 	model.addAttribute("estimatedTotal", estimatedTotal);
 	return "cart/shopping_cart";
