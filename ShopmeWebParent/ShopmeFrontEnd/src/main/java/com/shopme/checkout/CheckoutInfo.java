1fc5b94afb606d5fbf7d2aa8b90b2d40e73925b0
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-18 22:26:25 +0200

    Add functionality for processing payment with paypal

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutInfo.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutInfo.java
@@ -1,5 +1,6 @@
 package com.shopme.checkout;
 
+import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -54,6 +55,13 @@ public class CheckoutInfo {
 	}
 	public void setCodSupported(boolean codSupported) {
 		this.codSupported = codSupported;
+	}
+	
+	//Getting payment total for paypal ormatting 
+	//according to requirement for paypal
+	public String getPaymentTotal4PayPal() {
+		DecimalFormat formatter = new DecimalFormat("###,###.##");
+		return formatter.format(paymentTotal);
 	}
 	
 }
