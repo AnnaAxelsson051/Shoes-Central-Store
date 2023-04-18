12d9cee8fd0c51637a2923fabda35de21f244851
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-18 22:54:49 +0200

    Add test for paypal api

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/paypal/PayPalOrderResponse.java
@@ -0,0 +1,26 @@
+package com.shopme.checkout.paypal;
+
+public class PayPalOrderResponse {
+
+	private String id;
+	private String status;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	//Validation method for completed transaction 
+	//receives the id from the heckout page, 
+	public boolean validate(String orderId) {
+		return id.equals(orderId) && status.equals("COMPLETED");
+	}
+}
