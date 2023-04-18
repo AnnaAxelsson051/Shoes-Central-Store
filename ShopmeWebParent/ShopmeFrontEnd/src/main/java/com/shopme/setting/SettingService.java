069afc47a2c45a9ee5e5cbc221b01fd6766c91cc
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-18 19:08:52 +0200

    Add functionality for getting currency code

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/SettingService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/SettingService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.setting.Setting;
+import com.shopme.common.entity.Currency;
 import com.shopme.common.entity.setting.SettingCategory;
 
 
 @Service
 public class SettingService {
-	@Autowired private SettingRepository repo;
+	@Autowired private SettingRepository settingRepo;
+	@Autowired private CurrencyRepository currencyRepo;
 	
 	
 	//Returnmethod for the General settin object 
@@ -19,24 +21,33 @@ public class SettingService {
 	public List<Setting> getGeneralSettings() {
 		
 		
-		return repo.findByTwoCategories(SettingCategory.GENERAL, SettingCategory.CURRENCY);
+		return settingRepo.findByTwoCategories(SettingCategory.GENERAL, SettingCategory.CURRENCY);
 	}
 	
 	//Returning a new settingbag obj that takes the settings list collection
 	public EmailSettingBag getEmailSettings() {
-		List<Setting> settings = repo.findByCategory(SettingCategory.MAIL_SERVER);
-		settings.addAll(repo.findByCategory(SettingCategory.MAIL_TEMPLATES));
+		List<Setting> settings = settingRepo.findByCategory(SettingCategory.MAIL_SERVER);
+		settings.addAll(settingRepo.findByCategory(SettingCategory.MAIL_TEMPLATES));
 	
 	return new EmailSettingBag(settings);
 	}
 	
 	public CurrencySettingBag getCurrencySettings() {
-		List<Setting> settings = repo.findByCategory(SettingCategory.CURRENCY);
+		List<Setting> settings = settingRepo.findByCategory(SettingCategory.CURRENCY);
 	return new CurrencySettingBag(settings);
 	}
 	
 	public PaymentSettingBag getPaymentSettings() {
-		List<Setting> settings = repo.findByCategory(SettingCategory.PAYMENT);
+		List<Setting> settings = settingRepo.findByCategory(SettingCategory.PAYMENT);
 		return new PaymentSettingBag(settings);
+	}
+	
+	//Will return the code of the currency that is used
+	public String getCurrencyCode() {
+		Setting setting = settingRepo.findByKey("CURRENCY_ID");
+		Integer currencyId = Integer.parseInt(setting.getValue());
+		Currency currency = currencyRepo.findById(currencyId).get();
+		
+		return currency.getCode();
 	}
 }
