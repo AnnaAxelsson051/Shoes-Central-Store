acab9af0bbed82a2a5e675fd987e31afbfaaf18c
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-18 19:15:34 +0200

    Update checkout functionality with paymentsettings and currency code

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -24,6 +24,7 @@ import com.shopme.customer.CustomerService;
 import com.shopme.order.OrderService;
 import com.shopme.setting.CurrencySettingBag;
 import com.shopme.setting.EmailSettingBag;
+import com.shopme.setting.PaymentSettingBag;
 import com.shopme.setting.SettingService;
 import com.shopme.shipping.ShippingRateService;
 import com.shopme.shoppingcart.ShoppingCartService;
@@ -65,6 +66,13 @@ public class CheckoutController {
 		List<CartItem> cartItems = cartService.listCartItems(customer);
 		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
 		
+		String currencyCode = settingService.getCurrencyCode();
+		PaymentSettingBag paymentSettings = settingService.getPaymentSettings();
+		String paypalClientId = paymentSettings.getClientId();
+		
+		model.addAttribute("paypalClientId", paypalClientId);
+		model.addAttribute("currencyCode", currencyCode);
+		model.addAttribute("customer", customer);
 		model.addAttribute("checkoutInfo", checkoutInfo);
 		model.addAttribute("cartItems", cartItems);
 		
