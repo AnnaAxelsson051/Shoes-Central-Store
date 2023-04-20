8852ec89b5e3c0820964d58e622b18b10129e687
Anna Axelsson <a.axelsson51@gmail.com>
2023-04-20 12:56:32 +0200

    Add popup modal fr adding products

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -0,0 +1,8 @@
+//Showing the add product dialog
+$(document).ready(function(){
+	$("#products").on("click", "#linkAddProduct", function(e){
+		e.preventDefault();
+		$("#addProductModal").modal();
+		
+	})
+});
